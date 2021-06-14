06b5e069794ddfd53e3f124c0f88f3691e60af0c
Greg Neumann on his ChromeBook <GregInTheNorth@gmail.com>
2021-06-14 11:56:55 +0100

    Define basic structure at start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-reddit';
+addArticle(title: HTMLInputElement, link:HTMLInputElement): boolean{
+  console.warn(`About to add article with title ${title.value} and link ${link.value}`)
+ return false; 
+}
 }
